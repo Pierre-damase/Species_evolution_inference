a2d4645d8b6df75d0b528a29e6d99179c95df3ff
Pierre-damase <pierre.damase.imbert@hotmail.fr>
2021-02-22 11:34:14 +0100

    modif script migale

--- sei/sei_migale.sh
+++ sei/sei_migale.sh
@@ -4,16 +4,16 @@
 #$ -N dadi_opt
 
 # Number of separate submissions to the cluster
-#$ -t 1-41
+#$ -t 1-81
 
 # Short pour un job < 12h
 #$ -q short.q
 
 # Adresse à envoyer
-# -M pierre.imbert@college-de-france.fr
+#$ -M pierre.imbert@college-de-france.fr
 
-# Envoie mail - (e)nd & (a)bort
-# -m ea
+# Envoie mail - (b)egin, (e)nd, (a)bort & (s)uspend
+#$ -m as
 
 # Sortie standard
 #$ -o $HOME/work/Out
@@ -22,7 +22,5 @@
 #$ -e $HOME/work/Err
 
 conda activate sei-3.8.5
-python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py lrt --param tau --value $SGE_TASK_ID
-#python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py lrt --param kappa --value $SGE_TASK_ID
-#python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py lrt --param tau-kappa --value $SGE_TASK_ID $SGE_TASK_ID
+python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py lrt --param $1 --value $SGE_TASK_ID $SGE_TASK_ID
 conda deactivate
