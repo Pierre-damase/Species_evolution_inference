eec6df1069735256e13858211ab14af9433f594d
Pierre-damase <pierre.damase.imbert@hotmail.fr>
2021-06-11 17:15:46 +0200

    update migale script

--- sei/sei_migale.sh
+++ sei/sei_migale.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # Nom du job
-#$ -N opt_decline
-# -N opt_growth
-# -N opt_cst
-# -N smc_data
+# -N msprime
 
 # Number of separate submissions to the cluster
-#$ -t 1-11
-# -t 1-961
+#$ -t 1-4225
 
 # Short pour un job < 12h
 #$ -q long.q
@@ -26,10 +22,6 @@
 #$ -e $HOME/work/Err
 
 conda activate sei-3.8.5
-python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py optsmc --model decline --job $SGE_TASK_ID
-#python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py optsmc --model growth --job $SGE_TASK_ID
-#python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py optsmc --model cst --job $SGE_TASK_ID
-
-#python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py data --model decline --job $SGE_TASK_ID --typ vcf
+python /home/pimbert/work/Species_evolution_inference/sei/sei_migale.py data --model decline --job $SGE_TASK_ID --typ sfs
 
 conda deactivate
